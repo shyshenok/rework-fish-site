1a1217ab4b1de453db4300248b66f15ca161a972
shyshenok <shyshenoka@gmail.com>
2017-05-05 09:57:21 +0200

    Refactor to objects.

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("myCanvas");
-canvas.width  = window.innerWidth;
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
@@ -32,110 +32,91 @@ function Vector(x, y) {
     this.y = y;
 }
 
-Vector.prototype.add = function(v) {
+Vector.prototype.add = function (v) {
     this.x += v.x;
     this.y += v.y;
 };
 
-function MoveableObject(position,speed) {
-    this.position = position;
-    this.speed = speed;
+
+function Bezier3(p1, p2, p3) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
 }
 
-MoveableObject.prototype.draw = function(context) {
-    throw "Method draw is not implemented";
+Bezier3.prototype.bezier = function(t) {
+        return Math.pow((1 - t), 2) * this.p1 + 2 * (1 - t) * t * this.p2 + Math.pow(t, 2) * this.p3;
 };
 
-MoveableObject.prototype.step = function(width, height) {
-    throw "Method step is not implemented";
+Bezier3.prototype.derivative = function (t) {
+        return 2 * (1 - t) * (this.p2 - this.p1) + 2 * t * (this.p3 - this.p2);
 };
 
+function Bezier4(p1, p2, p3, p4) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+    this.p4 = p4;
+}
 
-function ImageObject (position, speed, src, scale) {
+Bezier4.prototype.bezier = function (t) {
+        return Math.pow((1 - t), 3) * this.p1 + 3 * Math.pow((1 - t), 2) * t * this.p2 + 3 * (1 - t) * Math.pow(t, 2) * this.p3 + Math.pow(t, 3) * this.p4;
+};
+Bezier4.prototype.derivative = function (t) {
+        return 3 * Math.pow((1 - t), 2) * (this.p2 - this.p1) + 6 * (1 - t) * t * (this.p3 - this.p2) + 3 * Math.pow(t, 2) * (this.p4 - this.p3);
+};
+
+function ImageObject(position, src, scale, bezierX, bezierY) {
     var self = this;
-    MoveableObject.apply(this, arguments);
+    this.position = position;
+
     this.img = new Image();
     this.img.src = src;
     this.img.onload = function () {
-        self.imgWidth = this.width*scale;
-        self.imgHeight = this.height*scale;
+        self.imgWidth = this.width * scale;
+        self.imgHeight = this.height * scale;
     };
+
     this.angle = 0;
+    this.bezierX = bezierX;
+    this.bezierY = bezierY;
 }
 
-ImageObject.prototype = Object.create(MoveableObject.prototype);
-ImageObject.prototype.constructor = ImageObject;
+ImageObject.prototype.step = function (t) {
+    this.position.x = this.bezierX.bezier(t);
+    this.position.y = this.bezierY.bezier(t);
 
-ImageObject.prototype.draw = function(context) {
+    var k = this.bezierX.derivative(t) / this.bezierY.derivative(t);
+    this.angle = (Math.atan(k) * 180 / Math.PI);
+};
+
+ImageObject.prototype.draw = function (context) {
     context.save();
     context.beginPath();
-    context.translate(this.position.x + this.imgWidth/2, this.position.y + this.imgHeight/2);
+    context.translate(this.position.x + this.imgWidth / 2, this.position.y + this.imgHeight / 2);
     context.rotate(this.angle * Math.PI / 180);
-    context.drawImage(this.img, -this.imgWidth/2, -this.imgHeight/2, this.imgWidth, this.imgHeight);
+    context.drawImage(this.img, -this.imgWidth / 2, -this.imgHeight / 2, this.imgWidth, this.imgHeight);
     context.closePath();
     context.restore();
 };
 
-ImageObject.prototype.step = function(width, height) {
-    if(this.position.x + this.speed.x > width || this.position.x + this.speed.x < 0) {
-        this.speed.x = -this.speed.x;
-    }
-    if(this.position.y + this.speed.y > height  || this.position.y + this.speed.y < 0) {
-        this.speed.y = -this.speed.y;
-    }
-
-    this.position.add(this.speed);
-};
-
-
-var firsImg = new ImageObject(
-    new Vector (canvas.width/2, canvas.height/2),
-    new Vector (1, -2),
+var firstImg = new ImageObject(
+    new Vector(canvas.width / 2, canvas.height / 2),
     "fish13.png",
-    1
+    1,
+    new Bezier3(200, 400, 800),
+    new Bezier3(400, 800, 400)
 );
 
+var secondImg = new ImageObject(
+    new Vector(canvas.width / 3, canvas.height / 3),
+    "fish13.png",
+    0.5,
+    new Bezier4(800, 640, 400, 380),
+    new Bezier4(400, 220, 100, 80)
+)
 
-function bezier3 (p1, p2, p3) {
-
-    return function(t) {
-        return Math.pow((1 - t), 2) * p1 + 2 * (1 - t) * t * p2 + Math.pow(t, 2) * p3;
-    }
-}
-
-function bezier3Der(p1, p2, p3) {
-    return function(t) {
-        return 2* (1 - t) *(p2 - p1) + 2* t * (p3 - p2);
-    }
-}
-
-function bezier4 (p1, p2, p3, p4) {
-
-    return function(t) {
-        return Math.pow((1 - t), 3) * p1 + 3 * Math.pow((1-t), 2) * t * p2 + 3 * (1-t) * Math.pow(t, 2)* p3 + Math.pow(t,3)* p4;
-    }
-}
-
-function bezier4Der (p1, p2, p3, p4) {
-
-    return function(t) {
-        return 3*Math.pow((1-t), 2) * (p2 - p1) + 6 * (1 - t) * t * (p3 - p2) + 3 * Math.pow(t, 2)* (p4 - p3);
-    }
-}
-
-var bez1x = bezier3(200, 400, 800);
-var bez1y = bezier3(400, 800, 400);
-
-var bez1xD = bezier3Der(200, 400, 800);
-var bez1yD = bezier3Der(400, 800, 400);
-
-var bez2x = bezier4(100, 340, 600, 780);
-var bez2y = bezier4(90, 420, 200, 400);
-
-var bez2xD = bezier4Der(100, 340, 600, 780);
-var bez2yD = bezier4Der(90, 420, 200, 400);
-
-function drawLine (context, bezierX, bezierY, numPoints) {
+function drawLine(context, bezierX, bezierY, numPoints) {
     var step = 1 / numPoints;
     var t = 0;
     context.fill();
@@ -152,10 +133,6 @@ function drawLine (context, bezierX, bezierY, numPoints) {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // ctx.moveTo(100, 90);
-    // drawLine(ctx, bez2x, bez2y, 300);
-    // firsImg.draw(ctx);
-    // firsImg.step(canvas.width, canvas.height);
 }
 
 setInterval(draw, 10);
@@ -165,12 +142,20 @@ animate({
     timing: function (timeFraction) {
         return timeFraction;
     },
-    draw: function(t) {
-        firsImg.position.x = bez2x(t);
-        firsImg.position.y = bez2y(t);
+    draw: function (t) {
+        firstImg.step(t);
+        firstImg.draw(ctx);
+    }
+});
 
-        var k = bez2xD(t) / bez2yD(t);
-        firsImg.angle = (Math.atan(k) * - 180 / Math.PI);
-        firsImg.draw(ctx);
+
+animate({
+    duration: 10000,
+    timing: function (timeFraction) {
+        return timeFraction;
+    },
+    draw: function (t) {
+        secondImg.step(t);
+        secondImg.draw(ctx);
     }
 });
